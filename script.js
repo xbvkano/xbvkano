4bf595d65c0e95c542d4358fd2132125cf4da4f2
Marcos Kano <71046148+xbvkano@users.noreply.github.com>
2023-08-04 03:37:15 +0000

    Create the certificate page with the template

--- script.js
+++ script.js
@@ -14,3 +14,11 @@ const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach(hid => {
     observer.observe(hid);
 });
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
